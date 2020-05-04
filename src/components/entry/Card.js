b69cf1930231a40a8052cd620192e62393606a7d
Baqar Ali <baqarali789@gmail.com>
2020-05-05 03:40:27 +0500

    Made changes

--- src/components/entry/Card.js
+++ src/components/entry/Card.js
@@ -53,7 +53,7 @@ const Card = ({OnRouteChange}) => {
                     </svg>
                 </a>
 
-                <a class="link dim gray hover-silver dib h2 w2 mr3"  href="mailto:baqarali789@gmail.com" title="Gmail" target="_newtab">
+                <a className="link dim gray hover-silver dib h2 w2 mr3"  href="mailto:baqarali789@gmail.com" title="Gmail" target="_newtab">
                     <svg style={{fill:'currentcolor'}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.069.432.27.268.431.643.431 1.068z"/></svg>
                 </a>
 
