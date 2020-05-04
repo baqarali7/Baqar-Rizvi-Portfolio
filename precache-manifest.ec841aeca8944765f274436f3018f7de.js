9db4274779f296ed5f32bae153b6ad116e84ea0f
Baqar Ali <baqarali789@gmail.com>
2020-05-05 04:40:09 +0500

    Updates

---
+++ precache-manifest.ec841aeca8944765f274436f3018f7de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6913bf5f587a49157f00741f926872a5",
+    "url": "/Baqar-Rizvi-Portfolio/index.html"
+  },
+  {
+    "revision": "4252c13e41108bdf68bb",
+    "url": "/Baqar-Rizvi-Portfolio/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "c1a1e6733a9feaaec606",
+    "url": "/Baqar-Rizvi-Portfolio/static/css/main.f89ebff7.chunk.css"
+  },
+  {
+    "revision": "4252c13e41108bdf68bb",
+    "url": "/Baqar-Rizvi-Portfolio/static/js/2.cf0dc45b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Baqar-Rizvi-Portfolio/static/js/2.cf0dc45b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a1e6733a9feaaec606",
+    "url": "/Baqar-Rizvi-Portfolio/static/js/main.7cbfd3ef.chunk.js"
+  },
+  {
+    "revision": "6769f23fb971bae60dc7",
+    "url": "/Baqar-Rizvi-Portfolio/static/js/runtime-main.051de583.js"
+  },
+  {
+    "revision": "9a04057b0f40f7cf788631df33b3c372",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/29.9a04057b.png"
+  },
+  {
+    "revision": "7271ced696536e7bfabab1b8bbff0479",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/Check.7271ced6.PNG"
+  },
+  {
+    "revision": "7c8c59dba846ea491a62a2b05b77631c",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/download.7c8c59db.jpg"
+  },
+  {
+    "revision": "c229f7866f6c217df5c504f7aaf84376",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/face.c229f786.PNG"
+  },
+  {
+    "revision": "3179e5a5894cfd8482f37abd8b51c6dc",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/pro.3179e5a5.jpg"
+  },
+  {
+    "revision": "8805e36be7d86ce47cba4b34b95d1239",
+    "url": "/Baqar-Rizvi-Portfolio/static/media/robo.8805e36b.PNG"
+  }
+]);
